c7e8ef2ac9f0e39d17a63961ae752e7090e9f721
MaagMichael <maagm@t-online.de>
2025-01-04 22:34:24 +0100

    footer icons

--- src/app/[locale]/components/Footer.tsx
+++ src/app/[locale]/components/Footer.tsx
@@ -1,6 +1,7 @@
 // import { useTranslations } from "next-intl"; //client side
 import { getTranslations } from "next-intl/server"; // server side with async and await
 import { Link } from "@/i18n/routing";
+import Image from "next/image";
 
 export default async function Footer() {
   const t = await getTranslations("Footer");
@@ -35,25 +36,29 @@ export default async function Footer() {
           <p className="text-xl">Follow Us</p>
 
           <div className="flex flex-row space-x-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              
+            <Link
+              href="https://www.instagram.com/casaripa.cupra/"
+              target="_blank"
             >
-              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-            </svg>
+              <Image
+                src="/assets/instagram-167.svg"
+                alt="Facebook"
+                width={24}
+                height={24}
+              />
+            </Link>
 
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              
+            <Link
+              href="https://www.facebook.com/pg/Casa-Ripa-Holiday-Home-110086234177655"
+              target="_blank"
             >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-            </svg>
+              <Image
+                src="/assets/facebook-176.svg"
+                alt="Facebook"
+                width={24}
+                height={24}
+              />
+            </Link>
           </div>
         </div>
       </div>
